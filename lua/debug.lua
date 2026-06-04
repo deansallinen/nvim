2da28cfec17b0ba7a35d68b6654a5317c30db0d2
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2026-06-04 01:04:21 -0700

    feat: create lua/debug.lua with DAP

---
+++ lua/debug.lua
@@ -0,0 +1,63 @@
+-- Debugging with DAP
+
+local function gh(repo)
+  return 'https://github.com/' .. repo
+end
+
+-- Dependencies
+vim.pack.add { gh 'nvim-neotest/nvim-nio' }
+vim.pack.add { gh 'rcarriga/nvim-dap-ui' }
+vim.pack.add { gh 'jay-babu/mason-nvim-dap.nvim' }
+vim.pack.add { gh 'leoluz/nvim-dap-go' }
+vim.pack.add { gh 'mfussenegger/nvim-dap' }
+
+local dap = require 'dap'
+local dapui = require 'dapui'
+
+-- Keymaps
+vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
+vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+vim.keymap.set('n', '<leader>B', function()
+  dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+end, { desc = 'Debug: Set Breakpoint' })
+vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+-- Mason DAP setup
+require('mason-nvim-dap').setup {
+  automatic_installation = true,
+  handlers = {},
+  ensure_installed = { 'delve' },
+}
+
+-- DAP UI setup
+dapui.setup {
+  icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+  controls = {
+    icons = {
+      pause = '⏸',
+      play = '▶',
+      step_into = '⏎',
+      step_over = '⏭',
+      step_out = '⏮',
+      step_back = 'b',
+      run_last = '▶▶',
+      terminate = '⏹',
+      disconnect = '⏏',
+    },
+  },
+}
+
+-- Auto open/close DAP UI
+dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+-- Go debugger
+require('dap-go').setup {
+  delve = {
+    detached = vim.fn.has 'win32' == 0,
+  },
+}
