0d5d2e1b7d3f42ba9fdb7a3e29c47412081752d4
Dean Sallinen <7519573+deansallinen@users.noreply.github.com>
2026-06-04 14:05:45 -0700

    fix: show attached LSP names in statusline, progress when active

--- plugin/00-plugins.lua
+++ plugin/00-plugins.lua
@@ -71,10 +71,16 @@ statusline.setup {
       local git = statusline.section_git { trunc_width = 40 }
       local diff = statusline.section_diff { trunc_width = 75 }
       local diagnostics = statusline.section_diagnostics { trunc_width = 75 }
-      local lsp = vim.lsp.status()
+      local lsp = statusline.section_lsp { trunc_width = 75 }
       local filename = statusline.section_filename { trunc_width = 140 }
       local fileinfo = statusline.section_fileinfo { trunc_width = 120 }
       local location = '%2l:%-2v'
+      
+      -- Show LSP progress if active, otherwise show attached LSP
+      local lsp_status = vim.lsp.status()
+      if lsp_status ~= '' then
+        lsp = lsp_status
+      end
 
       return statusline.combine_groups {
         { hl = mode_hl, strings = { mode } },
